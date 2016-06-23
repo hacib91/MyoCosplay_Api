8902486adbc9e2e253a23ac7114490e3ae5125a9
Haseeb <haseeb.khalid@epitech.eu>
2016-06-23 17:48:42 +0200

    test2

---
+++ REST.js
@@ -0,0 +1,192 @@
+var mysql = require("mysql");
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+    router.get("/",function(req,res){
+            res.json({"Message" : "API MYOCOsplay is working !"});
+    });
+	//add users
+    router.post("/users",function(req,res){
+       var query = "INSERT INTO ??(??,??,??,??,??,??,??,??,??,??) VALUES (?,?,?,?,?,?,?,?,?,?)";
+        var table = ["usr","login","mdp","echec_cnx","last_ip","last_co","device","email","telephone",
+		"nom","prenom",req.body.login,md5(req.body.mdp),0,'125.123','2015-10-5',req.body.device,
+		req.body.email,req.body.tel,req.body.nom,req.body.prenom];
+	
+		  //var table = ["usr","login", "mdp","email","telephone","nom","prenom",req.body.login,md5(req.body.mdp),req.body.email,req.body.telephone,req.body.nom,req.body.prenom,];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+               res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added ! "+req.body.email});
+				
+            }
+        });
+		
+		//end
+    });
+/*get users start*/
+	   router.get("/users",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["usr"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+	/*get users end*/
+	
+	/*get users with specfic id start*/
+	    router.get("/users/:id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["usr","id",req.params.id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+	
+	/*get users with specfic id end*/
+	
+	
+	/*get users start*/
+	    router.get("/users/:id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["usr","id",req.params.id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+	
+	/*get users end*/
+	/*update users start*/
+	router.put("/users",function(req,res){
+        var query = "UPDATE ?? SET ??=? WHERE ??=?";
+        var table = ["usr","mdp",md5(req.body.mdp),"email",req.body.email];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Updated the password for email "+req.body.email});
+            }
+        });
+    });
+	/*update users end*/
+	
+	
+	
+	/*delete users start*/
+	router.delete("/users/:email",function(req,res){
+        var query = "DELETE from ?? WHERE ??=?";
+        var table = ["usr","email",req.params.email];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Deleted the user with the id "+req.params.email});
+            }
+        });
+    });
+	/*delete users end*/
+	/*get news start*/
+	   router.get("/actu",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["actu"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Actu" : rows});
+            }
+        });
+    });
+	/*get news end*/
+	
+	
+	/*get news with specfic id start*/
+	    router.get("/actu/:id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["actu","id",req.params.id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+	
+	/*get news with specfic id end*/
+	
+	/*get event start*/
+	   router.get("/event",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["evenements"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Actu" : rows});
+            }
+        });
+    });
+	/*get event end*/
+	
+	
+	/*get events with specfic id start*/
+	    router.get("/event/:id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["evenements","id",req.params.id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+	
+	/*get event with specfic id end*/
+	
+	//add events
+    router.post("/event",function(req,res){
+       var query = "INSERT INTO ??(??,??,??,??,??) VALUES (?,?,?,?,?)";
+        var table = ["evenements","FR_titre","FR_contenu","FR_youtube","FR_cover_img","date", 
+		req.body.titre,req.body.contenu, req.body.youtube, req.body.cover,'2015-10-5'];
+	        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+               res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added ! "+req.body.contenu});
+				
+            }
+        });
+		 });
+		//add event
+		
+}
+
+module.exports = REST_ROUTER;
