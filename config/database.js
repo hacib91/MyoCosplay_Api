013988065646a13bb23bc9075e33a4c14cea3e51
Haseeb <haseeb.khalid@epitech.eu>
2017-01-04 21:59:30 +0100

    passport added

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': '163.172.28.97',
+        'user': 'yoann',
+        'password': 'baninou2!'
+    },
+	'database': 'yoann',
+    'users_table': 'usr'
+};
--- server.js
+++ server.js
@@ -1,18 +1,59 @@
-var express = require("express");
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
 var mysql   = require("mysql");
-var bodyParser  = require("body-parser");
-//var md5 = require('MD5');
-var sha1 = require('sha1');
-var hash = require('hash');
-var rest = require("./REST.js");
-var app  = express();
-//var session = require('express-session');
+var express  = require('express');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var app      = express();
+var sha1    = require('sha1');
+var port     = process.env.PORT || 8080;
+var rest = require("./app/REST.js");
+
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-// ok
+var flash    = require('connect-flash');
 
 
-//ok
+// configuration ===============================================================
+// connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+	secret: 'vidyapathaisalwaysrunning',
+	resave: true,
+	saveUninitialized: true
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
+
+
+
 function REST(){
     var self = this;
     self.connectMysql();
@@ -44,18 +85,6 @@ REST.prototype.configureExpress = function(connection) {
       var router = express.Router();
       app.use('/', router);
       var rest_router = new rest(router,connection,sha1);
-      self.startServer();
-}
-
-REST.prototype.startServer = function() {
-      app.listen(process.env.PORT || 5000 ,function(){
-          console.log("All right ! I am alive at Port 5000.");
-      });
-}
-
-REST.prototype.stop = function(err) {
-    console.log("ISSUE WITH MYSQL n" + err);
-    process.exit(1);
 }
 
 new REST();
