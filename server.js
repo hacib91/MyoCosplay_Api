615c73e31395f641a2addbf1b09fd2dc55d3df42
Haseeb <haseeb.khalid@epitech.eu>
2017-05-23 20:43:54 +0300

    orm online

--- server.js
+++ server.js
@@ -104,12 +104,29 @@ var Project = sequelize.define('myorm', {
     lastName: 'khalid'
   });
 });*/
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 app.use(restful(sequelize));
 /*MY ORMMMMMMMMMMMMMM */
 /*MY ORMMMMMMMMMMMMMM */
 /*MY ORMMMMMMMMMMMMMM */
 /*MY ORMMMMMMMMMMMMMM */
+
 
 REST.prototype.connectMysql = function() {
     var self = this;
